e53257aaa30e90cfd7d8b91915e605cffdc78443
Rafael Chaves <rafael@abstratt.com>
2018-03-20 10:18:12 -0300

    mostly reformatting

--- src/test/java/com/abstratt/kirra/spring/testing/sample/sample.kt
+++ src/test/java/com/abstratt/kirra/spring/testing/sample/sample.kt
@@ -11,7 +11,7 @@ import javax.persistence.*
 interface SampleMarker
 
 @SpringBootApplication
-open class ExpensesApplication : KirraJavaApplication(emptySet())
+open class SampleApplication : KirraJavaApplication(SampleRole.values().asIterable())
 
 
 enum class SampleRole : UserRole {
@@ -22,21 +22,21 @@ enum class SampleRole : UserRole {
 @Named(description = "Products that can be added to a car")
 class Product : BaseEntity() {
     @Column(nullable = false)
-    var name : String? = null
+    var name: String? = null
     @Column(nullable = false)
-    var price : Double? = null
+    var price: Double? = null
     @Column(nullable = true)
-    var category : String? = null
+    var category: String? = null
 }
 
 @Entity
-class OrderItem (
+class OrderItem(
         @ManyToOne(optional = false, cascade = arrayOf(CascadeType.ALL))
-        var order : Order? = null,
+        var order: Order? = null,
         @ManyToOne(optional = false)
-        var product : Product? = null,
+        var product: Product? = null,
         @Column(nullable = false)
-        var quantity : Int? = 1
+        var quantity: Int? = 1
 ) : BaseEntity() {
     val price get() = (this.product?.price ?: 0.0) * (this.quantity ?: 0)
 }
@@ -44,11 +44,11 @@ class OrderItem (
 @Entity
 class Order(override var id: Long? = null) : BaseEntity(id) {
     @ManyToOne(cascade = arrayOf(CascadeType.ALL), optional = false)
-    var customer : Customer? = null
+    var customer: Customer? = null
     @OneToMany(orphanRemoval = true, mappedBy = "order")
-    var items : MutableCollection<OrderItem> = ArrayList()
+    var items: MutableCollection<OrderItem> = ArrayList()
 
-    fun addItem(product : Product, quantity : Int) : OrderItem {
+    fun addItem(product: Product, quantity: Int): OrderItem {
         val newItem = OrderItem(order = this, product = product, quantity = quantity)
         items.add(newItem)
         return newItem
@@ -57,9 +57,9 @@ class Order(override var id: Long? = null) : BaseEntity(id) {
 
 @Entity
 abstract class Person(
-    override var id: Long? = null,
-    @Column(unique = true)
-    var name: String? = null
+        override var id: Long? = null,
+        @Column(unique = true)
+        var name: String? = null
 ) : RoleEntity(id)
 
 
@@ -77,23 +77,23 @@ class Customer : Person() {
 class Transfer(
         override var id: Long? = null,
         @ManyToOne
-    var source : Account? = null,
+        var source: Account? = null,
         @ManyToOne
-    var destination : Account? = null
+        var destination: Account? = null
 ) : BaseEntity(id)
 
 @Entity
 class Account(
         override var id: Long? = null,
         @ManyToOne
-    var owner : Customer? = null,
-        var balance : Double? = null,
-        @OneToMany(mappedBy="source")
-    var sent : Collection<Transfer>? = null,
-        @OneToMany(mappedBy="destination")
-    var received : Collection<Transfer>? = null,
+        var owner: Customer? = null,
+        var balance: Double? = null,
+        @OneToMany(mappedBy = "source")
+        var sent: Collection<Transfer>? = null,
+        @OneToMany(mappedBy = "destination")
+        var received: Collection<Transfer>? = null,
         @Enumerated(value = EnumType.STRING)
-    var type : AccountType? = null
+        var type: AccountType? = null
 ) : BaseEntity(id)
 
 enum class AccountType {
